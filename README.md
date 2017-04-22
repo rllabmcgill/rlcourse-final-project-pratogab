43dcd35c5944e6abe68025ce6d7a99eec5fec2c6
pratogab <prato@Gabrieles-MacBook-Pro.local>
2017-04-21 21:37:07 -0400

    Added paper and individual algorithms python files

--- README.md
+++ README.md
@@ -5,8 +5,13 @@
 
 **McGill University**
 
+paper.pdf is my submitted paper for the course project.
+
+presentation.pdf contains the slides that were presentated in class.
 
 Results shown in the presentation can be reproduce with:
 ```python
 python results.py
 ```
+
+dynaq.py, dynaqplus.py, greedydynaqplus.py, greedydynaqpluslog.py and greedydynaqplusucb.py contain the implemented algorithms described in the paper.
---
+++ greedydynaqpluslog.py
@@ -0,0 +1,164 @@
+import numpy as np
+
+class ControlAlgorithm:
+    def __init__(self, environment, ALPHA, DISCOUNT, EPSILON):
+        self.Q = np.zeros((environment.MAX_ROW + 1, environment.MAX_COLUMN + 1, ACTIONS.shape[0]))
+        self.environment = environment
+        self.environment.reset()
+        self.ALPHA = ALPHA
+        self.DISCOUNT = DISCOUNT
+        self.EPSILON = EPSILON
+      
+    def averageMTimesOverNSteps(self, M, N):
+        episodesPerTimeStep = np.zeros((M, N))
+        for i in range(M):
+            self.reset()
+            self.environment.reset()
+            episodesPerTimeStep[i] = self.runForNTimeSteps(N)
+
+        return np.average(episodesPerTimeStep, axis=0)
+    
+    def runForNTimeSteps(self, N):
+        stepCount = 0
+        cumulativeReward = 0
+        rewardsPerTimeStep = np.zeros(N)
+        
+        while stepCount < N:
+            episodeLength, episodeReward = self.generateEpisode()
+            cumulativeReward += episodeReward
+            
+            for i in range(stepCount, stepCount + episodeLength):
+                if i >= N:
+                    break
+                
+                rewardsPerTimeStep[i] = cumulativeReward
+                
+            stepCount += episodeLength
+        
+        return rewardsPerTimeStep
+    
+    def chooseAction(self):
+        if np.random.rand(1)[0] <= self.EPSILON:
+            actionId = np.random.randint(ACTIONS.shape[0], size=1)[0]
+        else:
+            actionId = np.argmax(self.Q[self.environment.state[0], self.environment.state[1]])
+            
+            # If multiple actions have the same max value, we need to choose one of them randomly
+            possibleActions = []
+            for i in range(ACTIONS.shape[0]):
+                if (self.Q[self.environment.state[0], self.environment.state[1], i] == 
+                   self.Q[self.environment.state[0], self.environment.state[1], actionId]):
+                    possibleActions.append(i)
+                    
+            actionId = random.choice(possibleActions)
+        
+        return ACTIONS[actionId], actionId
+    
+    # Displays the learned policy trying to complete an episode.
+    def viewLearnedPolicy(self):
+        self.environment.resetState()
+        self.environment.view()
+        
+        reward = 0
+        
+        while reward == 0:
+            previousState = np.copy(self.environment.state)
+            
+            action, actionId = self.chooseAction()
+            reward = self.environment.takeAction(action)
+            
+            clear_output()
+            self.environment.view()
+            time.sleep(0.5)
+
+class DynaQGreedyPlusLN(ControlAlgorithm):
+    def __init__(self, environment, ALPHA, DISCOUNT, N, K):
+        ControlAlgorithm.__init__(self, environment, ALPHA, DISCOUNT, 0)
+        # model stores for each state-action pair:
+        # 0 - Following state row
+        # 1 - Following state column
+        # 2 - Reward gotten
+        # 3 - Time steps since the action was last tried, in this state
+        # 4 - If this action was ever tried (0 no, 1 yes)
+        self.model = np.zeros((environment.MAX_ROW + 1, environment.MAX_COLUMN + 1, ACTIONS.shape[0], 5))
+        
+        self.triedStateActionPairs = []
+        
+        self.N = N
+        self.K = K
+        
+    def reset(self):
+        self.Q = np.zeros((self.environment.MAX_ROW + 1, self.environment.MAX_COLUMN + 1, ACTIONS.shape[0]))
+        self.model = np.zeros((self.environment.MAX_ROW + 1, self.environment.MAX_COLUMN + 1, ACTIONS.shape[0], 5))
+        self.triedStateActionPairs = []
+        
+    def generateEpisode(self):
+        self.environment.resetState()
+        
+        stepCount = 0
+        reward = 0
+        cumulativeReward = 0
+
+        while not np.array_equal(self.environment.state, self.environment.GOAL):
+            previousState = np.copy(self.environment.state)
+            
+            action, actionId = self.chooseAction()
+            reward = self.environment.takeAction(action)
+            cumulativeReward += reward
+        
+            self.Q[previousState[0], previousState[1], actionId] += self.ALPHA * (reward + 
+                self.DISCOUNT * np.max(self.Q[self.environment.state[0], self.environment.state[1]]) - 
+                self.Q[previousState[0], previousState[1], actionId])
+            
+            # We update the time since we last tried all previous state-action pairs
+            for i in range(environment.MAX_ROW + 1):
+                for j in range(environment.MAX_COLUMN + 1):
+                    for k in range(ACTIONS.shape[0]):
+                        self.model[i, j, k, 3] += 1
+            
+            self.model[previousState[0], previousState[1], actionId, 0] = self.environment.state[0]
+            self.model[previousState[0], previousState[1], actionId, 1] = self.environment.state[1]
+            self.model[previousState[0], previousState[1], actionId, 2] = reward
+            self.model[previousState[0], previousState[1], actionId, 3] = 0
+            
+            if self.model[previousState[0], previousState[1], actionId, 4] == 0:
+                self.triedStateActionPairs.append([previousState[0], previousState[1], actionId])
+                self.model[previousState[0], previousState[1], actionId, 4] = 1
+            
+            # Planning
+            for i in range(self.N):
+                stateActionPair = random.choice(self.triedStateActionPairs)
+                state = stateActionPair[0:2]
+                actionId = stateActionPair[2]
+                nextState = [0, 0]
+                nextState[0] = int(self.model[state[0], state[1], actionId, 0])
+                nextState[1] = int(self.model[state[0], state[1], actionId, 1])
+                reward = self.model[state[0], state[1], actionId, 2]
+                
+                self.Q[state[0], state[1], actionId] += self.ALPHA * (reward + 
+                    self.DISCOUNT * np.max(self.Q[nextState[0], nextState[1]]) - 
+                    self.Q[state[0], state[1], actionId])
+            
+            stepCount += 1
+        
+        return stepCount, cumulativeReward
+    
+    def chooseAction(self): # We override the usual chooseAction() function to implement the mentionned exploration bonus
+        maxValue = np.NINF
+        possibleActions = []
+        
+        for i in range(ACTIONS.shape[0]):
+            bonusReward = 0
+            if self.model[self.environment.state[0], self.environment.state[1], i, 3] != 0:
+                bonusReward = np.sqrt(np.log(self.model[self.environment.state[0], self.environment.state[1], i, 3]))
+                
+            if (self.Q[self.environment.state[0], self.environment.state[1], i] + self.K * bonusReward) > maxValue:
+                possibleActions = [i]
+                maxValue = self.Q[self.environment.state[0], self.environment.state[1], i] + self.K * bonusReward
+            elif (self.Q[self.environment.state[0], self.environment.state[1], i] + self.K * bonusReward) == maxValue:
+                possibleActions.append(i)
+                maxValue = self.Q[self.environment.state[0], self.environment.state[1], i] + self.K * bonusReward
+
+        actionId = random.choice(possibleActions)
+        
+        return ACTIONS[actionId], actionId
